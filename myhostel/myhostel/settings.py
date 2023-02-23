ad58618281384828df16ebbb8e49141a3471868b
Pritimayasahoo <pritimayasahoo1@gmail.com>
2023-02-23 17:07:18 +0530

    commit4

--- myhostel/myhostel/settings.py
+++ myhostel/myhostel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hh!t=h3ek6z(v9qf9_v!&d8xm2+309&*kn1=6m82tkg2c!dd!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
